ebd090da604a66110fc93b1505806a5a434a9ad0
np_cottaris <cottaris@sas.upenn.edu>
2015-07-02 16:28:19 -0400

    fixed case of  SelectScriptFromExistingOnes
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- cbValidateFullAndPublishOne.m
+++ cbValidateFullAndPublishOne.m
@@ -25,7 +25,7 @@ UnitTest.usePreferencesForProject('cbScripts', 'reset');
     UnitTest.setPref('graphMismatchedData', true);
 
     %% Print all existing validation scripts and ask the user to select one for validation
-    singleScriptToValidate = UnitTest.SelectScriptFromExistingOnes();
+    singleScriptToValidate = UnitTest.selectScriptFromExistingOnes();
     
     %% Validate
     UnitTest.runValidationSession({{singleScriptToValidate, []}}, 'PUBLISH');
