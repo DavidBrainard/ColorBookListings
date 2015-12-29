61ddb46c080094476fb16ff4eccc822650804b41
David Brainard <brainard@psych.upenn.edu>
2015-12-29 15:58:49 -0500

    Some data.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- cbUnitTestPreferences.m
+++ cbUnitTestPreferences.m
@@ -8,13 +8,6 @@
 % to that project's specifics.  You will want to change isetbioValidation
 % and isetbioRootPath and so forth.
 %
-% If you just run the distributed (Template) version to accept the
-% defaults, the FAST validation in isetbio will run.  But the FULL forms of
-% validation need the validation data. These can be found here:
-%
-%  http://????? - To be determined.
-%
-%
 % ISETBIO Team, 2015
 
 function cbUnitTestPreferences
@@ -24,7 +17,9 @@ function cbUnitTestPreferences
             'projectName',           'cbScripts', ...                                                                                 % The project's name (also the preferences group name)
             'validationRootDir',     '/Users/Shared/Matlab/Analysis/ColorBookListings', ...                                           % Directory location where the 'scripts' subdirectory resides.
             'alternateFastDataDir',  '',  ...                                                                                         % Alternate FAST (hash) data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/fast
-            'alternateFullDataDir',  '/Volumes/Users1/Shared/Matlab/Analysis/ColorBookListings/data/full', ...                                  % Alternate FULL data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/full
+            'alternateFullDataDir',  '/Volumes/Users1/Shared/Matlab/Analysis/ColorBookListings/data/full', ...                        % Alternate FULL data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/full
+            'useRemoteDataToolbox',  false, ...                                                                                       % If true use Remote Data Toolbox to fetch validation data on demand.
+            'remoteDataToolboxConfig', '/Users/dhb/Desktop/ProjectPrefs/isetbio/rdt-config-isetbio.json', ...                         % Struct, file path, or project name with Remote Data Toolbox configuration.
             'clonedWikiLocation',    '/Users/Shared/GitWebSites/ColorBookListings.wiki', ...                                          % Local path to the directory where the wiki is cloned. Only relevant for publishing tutorials.
             'clonedGhPagesLocation', '/Users/Shared/GitWebSites/ColorBookListings', ...                                               % Local path to the directory where the gh-pages repository is cloned. Only relevant for publishing tutorials.
             'githubRepoURL',         'https://davidbrainard.github.io/ColorBookListings', ...                                         % Github URL for the project. This is only used for publishing tutorials.
